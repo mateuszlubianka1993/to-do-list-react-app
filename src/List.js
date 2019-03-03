97a10094c1fcb0194ad0e7c97848bf86053537f2
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-03-03 23:20:43 +0100

    update

---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './List.css';
+
+const List = (props) => (
+    
+    <ul className="list-ul">
+       {props.tasks.map((task, index) => <li key={index}>Task number<span className="task-number"> {index+1}</span> {task}</li>)} 
+    </ul>
+);
+
+export default List;
