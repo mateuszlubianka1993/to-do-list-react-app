857a6642053a355b909ae20527767fff576ffb54
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-03-03 14:48:51 +0100

    add css to ToDoForm component

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './ToDoForm.css';
 
 class ToDoForm extends React.Component {
 
     render() {
         return (
             <div className="to-do-form-div">
-                <form onSubmit={this.props.addNewTask}>
+                <form onSubmit={this.props.addNewTask} className="to-do-form">
                     <input type="text" placeholder="Add new task" onChange={this.props.onChange}></input>
-                    <button>Add</button>
+                    <button className="add-button">Add</button>
                 </form>
                 <div className="buttons">
                     <button onClick={this.props.deleteAllTasks}>Delete All</button>
