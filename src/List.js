8bf3574b1692862cb64431562e9dbdbfdc400b4c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-03-28 19:13:54 +0100

    add animations

--- src/List.js
+++ src/List.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import './List.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class List extends React.Component{
 
     delete(index){
         this.props.delete(index);
     }
+
     
 render() {
+    
+    const listOfItems = this.props.tasks.map((task, index) => <li key={index}>
+    Task 
+    <span className="task-number"> 
+    {index+1}</span> 
+    {task}
+    <button onClick={this.delete.bind(this, task)} className="r-button"><i className="fas fa-minus-square"></i></button>
+</li>)
+
     return (
         <ul className="list-ul">
-            {this.props.tasks.map((task, index) => <li key={index}>
-                                                Task 
-                                                <span className="task-number"> 
-                                                {index+1}</span> 
-                                                {task}
-                                                <button onClick={this.delete.bind(this, task)} className="r-button"><i className="fas fa-minus-square"></i></button>
-                                        </li>)} 
+            <ReactCSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}>
+                {listOfItems}
+            </ReactCSSTransitionGroup>
+             
         </ul>
     );
 }
